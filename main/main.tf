2bf2dce2ee1d36b6020e967888d956431373f5d8
sashinfi <33483967+sashinfi@users.noreply.github.com>
2025-11-01 23:07:09 +0530

    Create main.tf
    adding main.tf

---
+++ main/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  # Configuration options
+  region = "${var.aws_region}"
+  access_key= "${var.aws_access_key}"
+  secret_key= "${var.aws_secret_key}"
+}
+
+
+module "ec2_instance" {
+  #aws_ec2_source
+  #"terraform-aws-modules/ec2-instance/aws"
+  source  ="terraform-aws-modules/ec2-instance/aws"
+  name = "sathish-ec2-instance"
+  ami = "${var.aws_ec2_ami}"
+  instance_type = "${var.aws_ec2_instance_type}"
+  key_name      = "${var.aws_keypair}"
+  monitoring    = true
+  subnet_id     = "${var.aws_ec2_subnet}"
+
+  tags = {
+    Name = "terraform-instance"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
