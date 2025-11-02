43137e1ea4df6041b8c7d0345a952edeae739102
sashinfi <33483967+sashinfi@users.noreply.github.com>
2025-11-02 09:03:08 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -11,8 +11,8 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "${var.aws_region}"
-  #access_key= "${var.aws_access_key}"
-  #secret_key= "${var.aws_secret_key}"
+  access_key= "${TF_VAR_access_key}"
+  secret_key= "${TF_VAR_access_key}"
 }
 
 
